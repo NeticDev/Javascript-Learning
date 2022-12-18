e0455c086dc3be3fc0b2e3f4dc1c17b247d362df
Neticz0r <112971004+Neticz0r@users.noreply.github.com>
2022-12-18 23:20:56 +0100

    challenge #1 functions

---
+++ Fundamentals Part 2/challenges.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgDolphins * 2 <= avgKoalas) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log(`No team wins.`);
+  }
+}
+
+/* const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49); */
+
+const avgDolphins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(23, 34, 27);
+
+checkWinner(avgDolphins, avgKoalas);
