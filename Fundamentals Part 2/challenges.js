2d14ab978d3c6f716b3a825c3cfb35576d78a8c0
Neticz0r <112971004+Neticz0r@users.noreply.github.com>
2023-01-02 14:29:59 +0100

    challenge #3

--- Fundamentals Part 2/challenges.js
+++ Fundamentals Part 2/challenges.js
@@ -48,3 +48,33 @@ console.log(total);
 
 // -------------------- CHALLENGE #3
 
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    return (this.BMI = this.mass / this.height ** 2);
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    return (this.BMI = this.mass / this.height ** 2);
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+mark.BMI > john.BMI
+  ? console.log(
+      `${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName}'s BMI (${john.BMI}).`
+    )
+  : console.log(
+      `${john.fullName}'s BMI (${john.BMI} is higher than ${mark.fullName}'s BMI (${mark.BMI}).`
+    );
